675a526b4e84d7840022da6f19b9318489d0e0e5
JinHui <1004260403@qq.com>
2017-10-18 20:42:13 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jh/note/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.jh.note;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 笔记项目 2017-10-18
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
